08d6caf59c7d0f4736aa1c4969db92121698e4d2
Mani Sai <35869499+manisaivecha341@users.noreply.github.com>
2018-06-10 10:33:52 +0530

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -7,76 +7,80 @@ from keras.utils.data_utils import get_file
 import numpy as np
 import random
 import sys
-#specify the path of the file where you clone all the reviews.
+
 path = 'reviewset.txt'
 text = open(path).read().lower()
-print('No of total characters in the dataset:', len(text))
+print('Total length:', len(text))
 
-ci = json.loads(open('ci.txt').read())
-ic = json.loads(open('ic.txt').read())
+char_indices = json.loads(open('ci.txt').read())
+indices_char = json.loads(open('ic.txt').read())
 chars = sorted(ci.keys())
-print('The final set of characters we encounter in the dataset are:')
-print(ic)
-print('total no of characters:', len(chars))
-maxlen = 512
+print(indices_char)
+print('total chars:', len(chars))
+maxlen = 256
 step = 3
-sentence_list = []
-followed_characters = []
+sentences = []
+next_chars = []
 for i in range(0, len(text) - maxlen, step):
-    sentence_list.append(text[i: i + maxlen])
-    followed_characters.append(text[i + maxlen])
-X = np.zeros((len(sentence_list), maxlen, len(chars)), dtype=np.bool)
-y = np.zeros((len(sentence_list), len(chars)), dtype=np.bool)
-for i, sequence in enumerate(sentence_list):
-    for t, char in enumerate(sequence):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('no of  sequences:', len(sentences))
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
         X[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
-print('Initializing modules.....')
+print('Initialising the model...')
 model = Sequential()
 model.add(LSTM(1024, return_sequences=True, input_shape=(maxlen, len(chars))))
 model.add(LSTM(512, return_sequences=False))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
-weight_updater= Adam(lr=0.002)
-model.compile(loss='categorical_crossentropy', optimizer=weight_updater)
-model.load_weights("update_weights")
-def sample(preds, temperature=0.6):
-    predicor= np.asarray(predictor).astype('float64')
-    predictor = np.log(predictor) / temperature
-    exp_preds = np.exp(predictor)
-    predictor = exp_preds / np.sum(exp_preds)
-    probabilities = np.random.multinomial(1, preds, 1)
-    return np.argmax(probabilities)
+optimizer = Adam(lr=0.002)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+model.load_weights("transfer_weights")
+
+def sample(preds, temperature=.6):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
 
 for iteration in range(1,60):
     print()
     print('-' * 50)
     print('Iteration', iteration)
     x = np.zeros((1, maxlen, len(chars)))
-    predictor = model.predict(x, verbose=0)[0]
+    preds = model.predict(x, verbose=0)[0]
     
     model.fit(X, y, batch_size=128, epochs=1)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
-    for specification in [0.2, 0.4, 0.6]:
+for diversity in [0.2, 0.4, 0.6, 0.8]:
         print()
-        print('----- specification:', specification)
+        print('----- diversity:', diversity)
 
         generated = ''
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
-        print('----- predcicting with the beginner: "' + sentence + '"')
+        print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
         for i in range(400):
             x = np.zeros((1, maxlen, len(chars)))
             for t, char in enumerate(sentence):
-                x[0, t, ci[char]] = 1.
+                x[0, t, char_indices[char]] = 1.
 
-            predictor = model.predict(x, verbose=0)[0]
-            next_index = sample(predictor, generated)
-            next_char = ic[str(next_index)]
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            #print(next_index)
+            #print (indices_char)
+            next_char = indices_char[str(next_index)]
 
             generated += next_char
             sentence = sentence[1:] + next_char
